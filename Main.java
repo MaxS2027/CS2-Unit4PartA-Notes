888c6329237269e7010c874e40de1fc767c99585
MaxS2027 <max.schwartz2027@gbwl.org>
2026-02-09 15:54:43 +0000

    commit

--- Main.java
+++ Main.java
@@ -2,5 +2,17 @@ public class Main {
 
    public static void main(String []args) {
       System.out.println("Hello World");
+
+      //1D ARRAY is a block of memory thatg stores a collection of data items(elements)
+      //Under one variable name
+
+      //how to DECLARE an array
+      String{} apCompSci = new String(5);
+      double[] prices = new double(5);
+      String[] names = new String(5);
+      int[] luckyNums = new int(5);
+      boolean[] yesNo = new boolean[3];
+
+      System.out.println( prices(0));
    }
 }
